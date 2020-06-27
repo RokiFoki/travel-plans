ae94a811697bbac5c48f04115543bc4f73564a0e
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-06-27 15:38:21 +0200

    add angular project

--- server.ts
+++ server.ts
@@ -1,12 +1,22 @@
 import express from 'express'
+import path from 'path';
 
 import config from './config';
+import bodyParser from 'body-parser';
+
+import apiRouter from './api/api';
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello world!');
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', apiRouter);
+
+app.use(express.static(__dirname + '/../angular/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../angular/dist/index.html'));
+});
 
 app.listen(config.PORT, function() {
     console.log(`Listening on port ${config.PORT}`);
