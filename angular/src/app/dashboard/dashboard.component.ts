13a9131697feb9ed399bb6c32d123e5d8ae79379
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-06-28 12:53:52 +0200

    add google maps

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -21,9 +21,24 @@ export class DashboardComponent implements OnInit {
   displayedColumns: string[] = ['destination', 'startDate', 'endDate', 'comment', 'startsIn', 'actions'];
   dataSource = ELEMENT_DATA;
 
+  map;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB5Xxxiq8p69G9HlVX9a6wgYILVLtvYrds';
+    script.defer = true;
+    script.async = true;
 
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+    };
+  }
 }
+
