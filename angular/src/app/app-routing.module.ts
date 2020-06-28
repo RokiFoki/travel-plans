b6963bf1231f388b8c5dc12051cdadce54d7e2f7
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-06-28 08:35:53 +0200

    installed material/flex, home, dashboard started

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+}, {
+  path: 'home',
+  redirectTo: ''
+}, {
+  path: 'dashboard',
+  loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
