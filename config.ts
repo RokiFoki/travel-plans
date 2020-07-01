f5d15f2ba3912f96e5dc28a6fa157e34561d52ae
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-07-01 17:56:00 +0200

    set up migrations. create roles table

--- config.ts
+++ config.ts
@@ -1,5 +1,10 @@
 const config = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    database: {
+        user: process.env.DATABASE_USER || 'postgres',
+        password: process.env.DATABASE_PASSWORD || 'postgres',
+        database: process.env.DATABASE || 'travel_plans'
+    }
 }
 
 export default config;
