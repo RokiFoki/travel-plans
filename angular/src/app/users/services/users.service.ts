e45edda87a25fcc3ceb66cedacc0efabd400078d
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-07-04 12:44:31 +0200

    getting users

---
+++ angular/src/app/users/services/users.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { RolesService } from './roles.service';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private users = new BehaviorSubject<User[]>([]);
+
+  constructor(
+    private http: HttpClient,
+    private rolesService: RolesService) { }
+
+  get() {
+    return this.users.asObservable();
+  }
+
+  fetch() {
+    this.http.get('/api/users').subscribe((data: any[]) => {
+      this.users.next(data.map(e => new User(e, this.rolesService)));
+    });
+  }
+
+  create(user: User) {
+    return this.http.post('/api/users', user)
+      .pipe(
+        map(e => {
+          return new User(e, this.rolesService);
+        }),
+        tap(u => {
+          const users = [...this.users.getValue()];
+          users.push(u);
+
+          this.users.next(users);
+        })
+      );
+  }
+
+  delete(userId: number) {
+    return this.http.delete('/api/users/' + userId)
+      .pipe(
+        tap(() => {
+          const users = [...this.users.getValue().filter(u => u.id !== userId)];
+          this.users.next(users);
+        })
+      );
+  }
+
+  edit(user: User) {
+    return this.http.put('/api/users/' + user.id, user)
+      .pipe(
+        tap(() => {
+          const users = [...this.users.getValue().map(u => u.id !== user.id ? u : user)];
+          this.users.next(users);
+        })
+      );
+  }
+}
+
+export class User {
+  public id: number;
+  public username: string;
+  public role: number;
+
+  constructor(
+    data: any,
+    private rolesService: RolesService) {
+      this.id = data.id;
+      this.username = data.username;
+      this.role = data.role;
+    }
+
+  roleName() {
+    return this.rolesService.getName(this.role);
+  }
+}
--- angular/src/app/users/users.component.ts
+++ angular/src/app/users/users.component.ts
@@ -1,52 +1,44 @@
-import { RolesService } from './services/roles.service';
+import { User, UsersService } from './services/users.service';
 import { AddNewUserDialogComponent } from './add-new-user-dialog/add-new-user-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { ConfirmDialogComponent } from '../dashboard/confirm-dialog/confirm-dialog.component';
 import { EditUserDialogComponent } from './edit-user-dialog/edit-user-dialog.component';
+import { Subscription } from 'rxjs';
 
-export class UserElement {
-  constructor(
-    public username: string,
-    public role: number,
-    public numberOfTrips: number,
-    private rolesService: RolesService) {}
-
-  roleName() {
-    return this.rolesService.getName(this.role);
-  }
-}
-
-const ELEMENT_DATA: UserElement[] = [];
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['username', 'role', 'actions'];
+  dataSource = new MatTableDataSource<User>();
 
-  displayedColumns: string[] = ['username', 'role', 'numberOfTrips', 'actions'];
-  dataSource = new MatTableDataSource<UserElement>(ELEMENT_DATA);
+  private subscription: Subscription;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
     private dialog: MatDialog,
-    private rolesService: RolesService) { }
+    private usersService: UsersService) { }
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource<UserElement>([
-    {numberOfTrips: 10, username: 'User1', role: 2},
-    {numberOfTrips: 20, username: 'User2', role: 1},
-    {numberOfTrips: 15, username: 'User3', role: 3},
-    {numberOfTrips: 22, username: 'User4', role: 3}].map(e => {
-      return new UserElement(e.username, e.role, e.numberOfTrips, this.rolesService);
-    }));
-
     this.dataSource.paginator = this.paginator;
+
+    this.subscription = this.usersService.get()
+      .subscribe(users => {
+        this.dataSource.data = users;
+      });
+
+    this.usersService.fetch();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   filterUsers(event: KeyboardEvent) {
