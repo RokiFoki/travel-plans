e0d1c4fafc941cd8aa118e91f579ad08613b63a9
Roko Krstulović <roko.krstulovic.opara@gmail.com>
2020-07-05 12:21:25 +0200

    authorisation interceptior for 401 errors

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthorizationInterceptor } from './authorization.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 
 import { SharedModule } from './shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,13 @@ import { SharedModule } from './shared/shared.module';
 
     MatMenuModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizationInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
